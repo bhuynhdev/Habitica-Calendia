1044bc2c04ef53b1ce93ec0507558d69259d40e2
Bao Huynh Lam <shinsusenju@aol.com>
2020-10-15 08:40:44 +0700

    Change static assets to build folder

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 app.use('/', indexRouter);
