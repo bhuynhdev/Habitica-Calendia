104e5fb6a8b13d218fd6aaf0524294cea052fe8f
Bao <huynhlbg@mail.uc.edu>
2022-10-18 13:49:12 -0400

    Add 'production' mode that serve static file of the built React client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect("http://localhost:3000");
+router.get("/", function (req, res, next) {
+  // Need to .trim() because https://stackoverflow.com/questions/39529870/ifprocess-env-node-env-production-always-false
+  const node_env = process.env.NODE_ENV.trim() || "development";
+  if (node_env === "development") {
+    return res.redirect("http://localhost:3000");
+  }
+  // Production
+  return res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,48 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var dotenv = require('dotenv');
-const cors = require('cors')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var dotenv = require("dotenv");
+const cors = require("cors");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var apiRouter = require("./routes/api");
+
 // Load config file
-dotenv.config({ path: './config/config.env'})
+dotenv.config({ path: "./config/config.env" });
 // Connect to MongoDB
-const connectDB = require('./db');
+const connectDB = require("./db");
 connectDB();
 
+console.log("Environment", process.env.NODE_ENV);
+
 const app = express();
-app.use(cors())
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api')
-
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.use("/", indexRouter);
+app.use("/api", apiRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', apiRouter);
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
